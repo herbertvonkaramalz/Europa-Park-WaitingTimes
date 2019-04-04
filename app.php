e7f41c9364ce599ae6b03497bad51ba2e93b85e4
unknown <dominik.myooran.ponniah@gmail.com>
2019-04-04 10:52:08 +0200

    New Design

--- app.php
+++ app.php
@@ -6,122 +6,166 @@ $time = date("YmdH", time());
 $token = strtoupper(implode(unpack("H*", hash_hmac("sha256", $time, $secret, true))));
 $waitingTimes = json_decode(file_get_contents($endpoint . "?token=" . $token), true);
 $link = $endpoint . "?token=" . $token;
-
 for($i = 0; $i < count($waitingTimes); $i++) {
-
-    /*
-    
-{"time":1,"code":600,"type":3,"status":0}, // Skandinavien
-
-
-
-{"time":0,"code":204,"type":3,"status":3}] // Irland
-    */
     switch($waitingTimes[$i]["code"]) {
-      /*Fehlt*/ case 853:
-            $waitingTimes[$i]['name'] = 'Wodan Timburcoaster';
-            break;
-      /*Fehlt*/  case 850:           
-             $waitingTimes[$i]['name'] = 'blue fire megacoaster';
-            break;
-        case 250:
-            $waitingTimes[$i]['name'] = 'Silver Star';
-            break;
-        case 200:
-            $waitingTimes[$i]['name'] = 'Eurosat - CanCanCoaster';
-            break;
-        case 500:
-            $waitingTimes[$i]['name'] = 'Euro-Mir';
-            break;
-        case 701:
-            $waitingTimes[$i]['name'] = 'Alpenexpress "Enzian"';
-            break;
-        case 351:
-            $waitingTimes[$i]['name'] = 'Matterhorn Blitz';
-            break;
-        case 403:
-            $waitingTimes[$i]['name'] = 'Pegasus';
-            break;
-        case 700:
-            $waitingTimes[$i]['name'] = 'Tiroler Wildwasserbahn';
-            break;
-        case 800:
-            $waitingTimes[$i]['name'] = 'Atlantica SuperSplash';
-            break;
-        case 400:
-            $waitingTimes[$i]['name'] = 'Poseidon';
-            break;
-        case 650:
-            $waitingTimes[$i]['name'] = 'Fjord Rafting';
-            break;
-        case 350:
-            $waitingTimes[$i]['name'] = 'Schweizer Bobbahn';
-            break;
-        case 851:
-            $waitingTimes[$i]['name'] = 'Whale Adventures - Splash Tours';
-            break;
-        case 404:
-            $waitingTimes[$i]['name'] = 'Abenteuer Atlantis';
-            break;
-        case 100:
-            $waitingTimes[$i]['name'] = 'Piraten in Batavia';
-            break;
-            /*Fehlt*/
-        case 550:
-            $waitingTimes[$i]['name'] = 'Madame Freudenreichs Curiositées';
-            break;
-            /* Fehlt*/
-       case 201:
-            $waitingTimes[$i]['name'] = 'Ba-a-a Express';
-            break;
-        case 157:
-            $waitingTimes[$i]['name'] = 'Dancing Dingie';
-            break;
-        case 159:
-            $waitingTimes[$i]['name'] = 'Dschungel-Flossfahrt';
-            break;
-        case 753:
-            $waitingTimes[$i]['name'] = 'Kolumbusjolle';
-            break;
-        case 155:
-            $waitingTimes[$i]['name'] = 'Old Mac Donald\'s Tractor Fun';
-            break;
-        case 901:
-            $waitingTimes[$i]['name'] = 'Poppy Tower';
-            break;
-        case 651:
-            $waitingTimes[$i]['name'] = 'Vindjammer';
-            break;
-        case 702:
-            $waitingTimes[$i]['name'] = 'Wiener Wellenflieger';
-            break;
-        case 9:
-            $waitingTimes[$i]['name'] = 'Voletarium';
-            break;
-        case 900:
-            $waitingTimes[$i]['name'] = 'Arthur - Im Königreich der Minimoys';
-            break;
-        case 202:
-            $waitingTimes[$i]['name'] = 'Euro-Tower';
-            break;
-        case 495:
-            $waitingTimes[$i]['name'] = 'Arena of Football';
-            break;
-        case 102:
-            $waitingTimes[$i]['name'] = 'Volo da Vinci';
-            break;
+      case 853:
+        $waitingTimes[$i]['name'] = 'Wodan Timburcoaster';
+        $waitingTimes[$i]['country'] = 'ISL';
+      break;
+      case 850:
+        $waitingTimes[$i]['name'] = 'blue fire megacoaster';
+        $waitingTimes[$i]['country'] = 'ISL';
+        break;
+      case 250:
+        $waitingTimes[$i]['name'] = 'Silver Star';
+        $waitingTimes[$i]['country'] = 'FRA';
+        break;
+      case 200:
+        $waitingTimes[$i]['name'] = 'Eurosat CanCan Coaster';
+        $waitingTimes[$i]['country'] = 'FRA';
+        break;
+      case 500:
+        $waitingTimes[$i]['name'] = 'Euro-Mir';
+        $waitingTimes[$i]['country'] = 'RUS';
+        break;
+      case 701:
+        $waitingTimes[$i]['name'] = 'Alpenexpress "Enzian"';
+        $waitingTimes[$i]['country'] = 'AUT';
+        break;
+      case 351:
+        $waitingTimes[$i]['name'] = 'Matterhorn Blitz';
+        $waitingTimes[$i]['country'] = 'SUI';      
+        break;
+      case 403:
+        $waitingTimes[$i]['name'] = 'Pegasus';
+        $waitingTimes[$i]['country'] = 'GRE';      
+        break;
+      case 700:
+        $waitingTimes[$i]['name'] = 'Tiroler Wildwasserbahn';
+        $waitingTimes[$i]['country'] = 'AUT';
+        break;
+      case 800:
+        $waitingTimes[$i]['name'] = 'Atlantica SuperSplash';
+        $waitingTimes[$i]['country'] = 'POR';      
+        break;
+      case 400:
+        $waitingTimes[$i]['name'] = 'Poseidon';
+        $waitingTimes[$i]['country'] = 'GRE';      
+        break;
+      case 650:
+        $waitingTimes[$i]['name'] = 'Fjord Rafting';
+        $waitingTimes[$i]['country'] = 'SKA';      
+        break;
+      case 350:
+        $waitingTimes[$i]['name'] = 'Schweizer Bobbahn';
+        $waitingTimes[$i]['country'] = 'SUI';      
+        break;
+      case 851:
+        $waitingTimes[$i]['name'] = 'Whale Adventures - Splash Tour';
+        $waitingTimes[$i]['country'] = 'ISL';      
+        break;
+      case 404:
+        $waitingTimes[$i]['name'] = 'Abenteuer Atlantis';
+        $waitingTimes[$i]['country'] = 'GRE';      
+        break;
+      case 100:
+        $waitingTimes[$i]['name'] = 'Geisterschloss';
+        $waitingTimes[$i]['country'] = 'ITA';      
+        break;
+      case 550:
+        $waitingTimes[$i]['name'] = 'Piraten in Batavia';
+        $waitingTimes[$i]['country'] = 'NED';      
+        break;
+      case 201:
+        $waitingTimes[$i]['name'] = 'Madame Freudenreich Curiositées';
+        $waitingTimes[$i]['country'] = 'FRA';      
+        break;
+      case 157:
+        $waitingTimes[$i]['name'] = 'Ba-a-a Express';
+        $waitingTimes[$i]['country'] = 'IRL';      
+        break;
+      case 159:
+        $waitingTimes[$i]['name'] = 'Dancing Dingie';
+        $waitingTimes[$i]['country'] = 'IRL';      
+        break;
+      case 600:
+        $waitingTimes[$i]['name'] = 'Dschungel-Floßfahrt';
+        $waitingTimes[$i]['country'] = 'EU';      
+        break;
+      case 753:
+        $waitingTimes[$i]['name'] = 'Kolumbusjolle';
+        $waitingTimes[$i]['country'] = 'ESP';
+        break;
+      case 155:
+        $waitingTimes[$i]['name'] = 'Old Mac Donald\'s Traktorfun';
+        $waitingTimes[$i]['country'] = 'IRL';      
+        break;
+      case 901:
+        $waitingTimes[$i]['name'] = 'Poppy Tower';
+        $waitingTimes[$i]['country'] = 'EU';     
+        break;
+      case 651:
+        $waitingTimes[$i]['name'] = 'Vindjammer';
+        $waitingTimes[$i]['country'] = 'SKA';      
+        break;
+      case 702:
+        $waitingTimes[$i]['name'] = 'Wiener Wellenflieger';
+        $waitingTimes[$i]['country'] = 'AUT';
+        break;
+      case 9:
+        $waitingTimes[$i]['name'] = 'Voletarium';
+        $waitingTimes[$i]['country'] = 'GER';
+        break;
+      case 900:
+        $waitingTimes[$i]['name'] = 'Arthur - Im Königreich der Minimoys';
+        $waitingTimes[$i]['country'] = 'EU';
+        break;
+      case 202:
+        $waitingTimes[$i]['name'] = 'Euro-Tower';
+        $waitingTimes[$i]['country'] = 'FRA';
+        break;
+      case 495:
+        $waitingTimes[$i]['name'] = 'Arena of Football';
+        $waitingTimes[$i]['country'] = 'GBR';      
+        break;
+      case 102:
+        $waitingTimes[$i]['name'] = 'Volo da Vinci';
+        $waitingTimes[$i]['country'] = 'ITA';
+        break;
+      case 204:
+        $waitingTimes[$i]['name'] = 'Jim Knopf - Reise nach Lummerland';
+        $waitingTimes[$i]['country'] = 'FRA';
+        break;
     }
-}
-?>
+  } 
+  
+  ?>
+
 <!DOCTYPE html>
 <html lang="de">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title>Wartezeiten</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./flags.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
 <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+
+<style>
+.flag {
+   /* background-image: url('https://cdn.countryflags.com/thumbs/switzerland/flag-round-250.png');*/
+    width: 45px;
+    height: 45px;
+    margin-top: -3px;
+    margin-left: -3px;
+    background-size: auto 100%;
+    background-repeat: no-repeat;
+    background-position: left top;
+
+/* <img src="https://cdn.countryflags.com/thumbs/switzerland/flag-round-250.png" width="47px" height="47px" style="margin-top: -18px; margin-left: -4px;"/> */
+}
+</style>
     </head>
     <body>
     <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
@@ -137,29 +181,45 @@ for($i = 0; $i < count($waitingTimes); $i++) {
     <ul class="demo-list-controll mdl-list">
     <?php
 		    for($i = 0; $i < count($waitingTimes); $i++) {
-                $badgeString = "success";
+                $badgeStatus = "lightgreen";
+
   
                 if($waitingTimes[$i]['time'] > 5) {
-                  $badgeString = "warning";
+                  $badgeStatus = "red";
                 } 
   
                 if($waitingTimes[$i]['time'] >= 20) {
-                  $badgeString = "danger";
+                  $badgeStatus = "yellow";
                 }
   
                 if($waitingTimes[$i]['status'] != 0) {
-                  $badgeString = "default";
+                  $badgeStatus = "grey; color: white";
+                }
+
+                if($waitingTimes[$i]['time'] == 0) {
+                    $timeStamp = 'Keine Wartezeit';
+                } else if ($waitingTimes[$i]['time'] == 1) {
+                    $timeStamp = '1 Minute';
+                } else if ($waitingTimes[$i]['time'] == 60) {
+                    $timeStamp = '1 Stunde';
+                } else if ($waitingTimes[$i]['time'] == 120) {
+                    $timeStamp = '2 Stunden';
+                } else {
+                    $timeStamp = $waitingTimes[$i]['time'] . ' Minuten';
                 }
 
             echo '
 
                 <li class="mdl-list__item">
                     <span class="mdl-list__item-primary-content">
-                        <i class="material-icons mdl-list__item-avatar">person</i>
-                        ' . $waitingTimes[$i]['name'] .'
+                        <i class="material-icons mdl-list__item-avatar">
+                        <div class="flag-'. $waitingTimes[$i]['country']. '"></div></i>
+                        ' . $waitingTimes[$i]['name'] . '
                     </span>
                     <span class="mdl-list__item-secondary-action">
-                        ' . $waitingTimes[$i]['time'] . ' Minuten
+                        <span class="mdl-chip" style="background-color: '. $badgeStatus .';">
+                            <span class="mdl-chip__text">' . $timeStamp .'</span>
+                        </span>
                     </span>
                 </li>';      
             }
@@ -170,17 +230,3 @@ for($i = 0; $i < count($waitingTimes); $i++) {
 </div>
     </body>
 </html>
-
-<ul class="demo-list-control mdl-list">
-<li class="mdl-list__item">
-  <span class="mdl-list__item-primary-content">
-    <i class="material-icons  mdl-list__item-avatar">person</i>
-    Bryan Cranston
-  </span>
-  <span class="mdl-list__item-secondary-action">
-    <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="list-checkbox-1">
-      <input type="checkbox" id="list-checkbox-1" class="mdl-checkbox__input" checked />
-    </label>
-  </span>
-</li>
-</ul>
